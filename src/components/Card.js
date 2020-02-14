dc05ce17681e978d0210638dfd99b3c99420265c
Jun <lhcjun@gmail.com>
2020-02-14 20:05:39 +0800

    add img

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from "react";
 
-
-
-const Card = ({name, email, bs, id}) => {
-    return (
-        <div>
-            <img src={"./img/1.png"}  alt="photo"/>
-            <div>
-                <h2>{ name }</h2>
-            </div>
-        </div>
-    )
-}
+const Card = ({ name, email, bs, id }) => {
+  return (
+    <div className="tc grow br3 pa3 ma2 dib bw2 shadow-5">
+      <img alt="member" src={`../img/1.png`} />
+      <div>
+        <h2>{name}</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
